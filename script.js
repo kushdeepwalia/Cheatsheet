c87caf4f04963b97fbf04353ffffa262391b9efd
KushdeepSingh <skushdeep5@gmail.com>
2021-01-24 14:12:03 +0530

    added php css and js content

--- script.js
+++ script.js
@@ -21,33 +21,78 @@ listItems.forEach(element => {
     })
 });
 
-fetch("https://spreadsheets.google.com/feeds/list/1bHRdk1vAp_Q5_8moWbrlUJHb48pXsmprwXnw33Iy2B8/1/public/full?alt=json")
+fetch("https://spreadsheets.google.com/feeds/list/1iAZRBOzGeX7pbxLKEVtl9or_eCXYwwXCuF-sONU3BWM/1/public/full?alt=json")
 .then((res)=>res.json())
 .then((data)=>{
     const htmlContent = document.getElementById("htmlContent");
+    const cssContent = document.getElementById("cssContent");
+    const jsContent = document.getElementById("jsContent");
+    const phpContent = document.getElementById("phpContent");
     var totalCodes = data["feed"]["entry"].length;
     var code = data["feed"]["entry"];
     let htmlCode = "";
+    let cssCode = "";
+    let jsCode = "";
+    let phpCode = "";
     for(let i=0;i<totalCodes;i++)
     {
         if(code[i]["gsx$language"]["$t"]==="HTML")
         {
-            htmlCode+=`<li>
-            <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
-            <pre>
-                <code class="language-html" id="test${i+1}">&lt;p&gt;&lt;/p&gt;</code>
-            </pre>
-        </li>`
+            htmlCode+=`
+            <br>
+            <li>
+                <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+                <pre>
+                    <code class="language-html" id="test${i+1}"></code>
+                </pre>
+            </li>`
+        }
+        if(code[i]["gsx$language"]["$t"]==="CSS")
+        {
+            cssCode+=`
+            <br>
+            <li>
+                <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+                <pre>
+                    <code class="language-css" id="test${i+1}"></code>
+                </pre>
+            </li>`
+        }
+        if(code[i]["gsx$language"]["$t"]==="JavaScript")
+        {
+            jsCode+=`
+            <br>
+            <li>
+                <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+                <pre>
+                    <code class="language-js" id="test${i+1}"></code>
+                </pre>
+            </li>`
+        }
+        if(code[i]["gsx$language"]["$t"]==="PHP")
+        {
+            phpCode+=`
+            <br>
+            <li>
+                <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+                <pre>
+                    <code class="language-php" id="test${i+1}"></code>
+                </pre>
+            </li>`
         }
     }
-    htmlContent.innerHTML=htmlCode;
+    if(htmlCode !== "")
+        htmlContent.innerHTML=htmlCode;
+    if(cssCode !== "")
+        cssContent.innerHTML=cssCode;
+    if(jsCode !== "")
+        jsContent.innerHTML=jsCode;
+    if(phpCode !== "")
+        phpContent.innerHTML=phpCode;
     for(let i=0;i<totalCodes;i++)
     {
-        if(code[i]["gsx$language"]["$t"]==="HTML")
-        {
-            let j = i+1;
-            document.getElementById("test"+j).textContent=code[i]["gsx$code"]["$t"];
-        }
+        let j = i+1;
+        document.getElementById("test"+j).textContent=code[i]["gsx$code"]["$t"];
     }
 })
 .then(() => {
