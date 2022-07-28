f86b523aec9d6546bf37e2d79dc997349a4e109b
Kushdeep Walia <skushdeep5@gmail.com>
2022-07-29 03:20:58 +0530

    code added

--- js/script.js
+++ js/script.js
@@ -41,50 +41,50 @@ fetch(api)
         if(code[i][1]==="HTML")
         {
             cheatCode+=`<br><li>
-            <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+            <span class="heading">${code[i][2]}</span>
             <pre class="language-html">
                 <code class="language-html" id="htmlText${i+1}"></code>
             </pre>
             </li>`
             htmlContent.innerHTML+=cheatCode;            
             let text = document.getElementById("htmlText"+j);
-            text.textContent+=code[i]["gsx$code"]["$t"];
+            text.textContent+=code[i][3];
         }
         else if(code[i][1]==="CSS")
         {
             cheatCode+=`<br><li>
-            <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+            <span class="heading">${code[i][2]}</span>
             <pre class="language-css">
                 <code class="language-css" id="cssText${i+1}"></code>
             </pre>
             </li>`
             cssContent.innerHTML+=cheatCode;
             let text = document.getElementById("cssText"+j);
-            text.textContent+=code[i]["gsx$code"]["$t"];
+            text.textContent+=code[i][3];
         }
         else if(code[i][1]==="JavaScript")
         {
             cheatCode+=`<br><li>
-            <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+            <span class="heading">${code[i][2]}</span>
             <pre class="language-js">
                 <code class="language-js" id="jsText${i+1}"></code>
             </pre>
             </li>`
             jsContent.innerHTML+=cheatCode;
             let text = document.getElementById("jsText"+j);
-            text.textContent+=code[i]["gsx$code"]["$t"];
+            text.textContent+=code[i][3];
         }
         else if(code[i][1]==="PHP")
         {
             cheatCode+=`<br><li>
-            <span class="heading">${code[i]["gsx$heading"]["$t"]}</span>
+            <span class="heading">${code[i][2]}</span>
             <pre class="language-php">
                 <code class="language-php" id="phpText${i+1}"></code>
             </pre>
             </li>`
             phpContent.innerHTML+=cheatCode;
             let text = document.getElementById("phpText"+j);
-            text.textContent+=code[i]["gsx$code"]["$t"];
+            text.textContent+=code[i][3];
         }
     }
 })
